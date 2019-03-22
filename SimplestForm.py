6c42ab50654c0b9d25681ab95b4647852311a6ed
Cephas Kevin Nagalla <33223869+CephasKevin@users.noreply.github.com>
2019-03-22 21:43:51 +0000

    Add files via upload

--- SimplestForm.py
+++ SimplestForm.py
@@ -45,18 +45,23 @@ def simplifyFraction(numer, denom):
 
 while simplificationMade == "true":
     
-    global simplifiedNumer
-    global simplifiedDenom
-
+    #You only need use the global keyword if calling an external variable from with in a method!
     preSimplifiedNumer = simplifiedNumer
+
     preSimplifiedDenom = simplifiedDenom
 
     if simplifiedNumer != 0 or simplifiedDenom != 0:
         simplifyFraction(simplifiedNumer, simplifiedDenom)
       
     else:
-        
-        simplifyFraction(-1080, 240)#ENTER NUMERATOR AND DENOMATOR HERE RESPECTIVELY
+        #ENTER NUMERATOR AND DENOMATOR HERE 
+        Numerator = -5788
+        Denomenator = -1268
+
+        if Numerator and Denomenator < 0:#A NEGATIVE DIVIDED BY A NEGATIVE IS A POSITIVE!
+            simplifyFraction(Numerator * -1, Denomenator * -1)
+        else:
+            simplifyFraction(Numerator, Denomenator)   
 
     if preSimplifiedNumer == simplifiedNumer or simplifiedNumer == 2 or preSimplifiedDenom == simplifiedDenom or simplifiedDenom == 2:
         simplificationMade = "false"         
@@ -69,9 +74,4 @@ else:
     print("simplified Denomenator = " + str(simplifiedDenom))    
 
 
-    
-   
-
-
-print("")
-print("Python Ended")
+print("/nPython Ended")
